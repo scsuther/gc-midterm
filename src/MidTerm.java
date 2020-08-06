e5193359a2acad7a4e157c0bc986c89b4033e3be
scsuther <scsuther@live.com>
2020-08-06 10:36:01 -0400

    scotts commit

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -6,6 +6,11 @@ public class MidTerm {
 				"Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
 		
 		System.out.println("They were very clever monkeys indeed, known for their ingenious teamwork. Once they were challenged by a swarm of seemingly unstoppable bugs.");
+	
+		System.out.println("But following Kai-Yun’s unifying leadership, they paired up and mowed down their foes. One-by-one every last pesky bug fell at the quick hands of the monkeys.\r\n" + 
+				"");
+	
+	
 	}
 
 }
