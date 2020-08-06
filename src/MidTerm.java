5ef28adb164998bdeb7475fdea986bcca57a21fa
maisiegallagher <maisiejgallagher@gmail.com>
2020-08-06 13:54:06 -0400

    maisie's commit

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -3,6 +3,8 @@ public class MidTerm {
 
 	public static void main(String[] args) {
 		
+		// System.out.println("Welcome to  Lakshmi's Market!");
+		// System.out.println("Please choose an item from the menu below: ");
 		
 	}
 
