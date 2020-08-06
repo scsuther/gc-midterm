fb1310d33ec83c1e51b927aad1b711fda340daa1
maisiegallagher <maisiejgallagher@gmail.com>
2020-08-06 10:32:49 -0400

    second commit

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -4,6 +4,8 @@ public class MidTerm {
 	public static void main(String[] args) {
 		System.out.println(
 				"Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
+		
+		System.out.println("They were very clever monkeys indeed, known for their ingenious teamwork. Once they were challenged by a swarm of seemingly unstoppable bugs.");
 	}
 
 }
